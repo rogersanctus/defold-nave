ca510f577aeb92d3569a59c880bc2d81bb23e95c
Rogério Santos <rogersanctus@gmail.com>
2024-05-04 23:28:45 -0300

    Implements a infinite vertical scrolling background

---
+++ assets/main_level.go
@@ -0,0 +1,30 @@
+components {
+  id: "background_tilemap"
+  component: "/assets/background.tilemap"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "scrolling_background"
+  component: "/main/scripts/scrolling_background.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
